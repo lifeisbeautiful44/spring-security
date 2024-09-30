21e1deec90c52fbbcbbe431b32f566f285d1d458
lifeisbeautiful44 <bohara.srijansil44@gmail.com>
2024-09-30 22:50:45 +0545

    Added Password Encoder and UserDetailService to save and retrive the user from the inmemoryuserdetail manager

--- section3/springsec-section3/src/main/java/com/lifeIsbeautiful/controller/config/SecurityConfig.java
+++ section3/springsec-section3/src/main/java/com/lifeIsbeautiful/controller/config/SecurityConfig.java
@@ -3,6 +3,12 @@ package com.lifeIsbeautiful.controller.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,5 +26,27 @@ public class SecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService getUserDetails()
+    {
+        UserDetails user = User.withUsername("srijansil")
+                .password("{noop}12345")
+                .authorities("read")
+                .build();
+
+        UserDetails admin = User.withUsername("luffy")
+                .password("{bcrypt}$2a$12$/PXJDuIyhDMI/Le8IGcPG.RXb4OraMsLuhCMkDyTG0FUFD6IJe746")
+                .authorities("admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder()
+    {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
