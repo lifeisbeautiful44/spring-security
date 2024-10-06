61af58c92d96e789084f2a3f9838c9de58f4b44a
lifeisbeautiful44 <bohara.srijansil44@gmail.com>
2024-10-06 22:13:15 +0545

    Section 4 added

---
+++ section4/springsec-section4/src/main/java/com/lifeIsbeautiful/LifeisBeautifulBankApplication.java
@@ -0,0 +1,13 @@
+package com.lifeIsbeautiful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeisBeautifulBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeisBeautifulBankApplication.class, args);
+	}
+
+}
---
+++ section4/springsec-section4/src/main/java/com/lifeIsbeautiful/controller/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.lifeIsbeautiful.controller.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount","/myBalance","/myCards","/myLoans").authenticated()
+                .requestMatchers("/contact","/notices").permitAll());
+
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService getUserDetails(DataSource dataSource)
+    {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder()
+    {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker passwordChecker()
+    {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
