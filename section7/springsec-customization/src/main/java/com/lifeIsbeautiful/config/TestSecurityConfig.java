810b883d442d79fea1ed3731aef27d0222b80c52
lifeisbeautiful44 <bohara.srijansil44@gmail.com>
2024-11-27 22:55:30 +0545

    sec 7 common spring-secuity customization.

---
+++ section7/springsec-customization/src/main/java/com/lifeIsbeautiful/config/TestSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.lifeIsbeautiful.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("!prod")
+public class TestSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.requiresChannel(rrc -> rrc.anyRequest().requiresInsecure()) //only HTTP
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+                        .requestMatchers("/contact", "/notices", "/register").permitAll());
+
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker passwordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
