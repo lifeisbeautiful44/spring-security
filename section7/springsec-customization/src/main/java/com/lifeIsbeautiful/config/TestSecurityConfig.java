21f0b5ac3748248e1360b830af3e5c67119c416a
lifeisbeautiful44 <bohara.srijansil44@gmail.com>
2024-12-02 22:20:11 +0545

    AuthenticationEntryPoint Defined.

--- section7/springsec-customization/src/main/java/com/lifeIsbeautiful/config/TestSecurityConfig.java
+++ section7/springsec-customization/src/main/java/com/lifeIsbeautiful/config/TestSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.lifeIsbeautiful.config;
 
+import com.lifeIsbeautiful.exception.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -26,7 +27,7 @@ public class TestSecurityConfig {
                         .requestMatchers("/contact", "/notices", "/register").permitAll());
 
         http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.httpBasic(hcc -> hcc.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
         return http.build();
     }
 
---
+++ section7/springsec-customization/src/main/java/com/lifeIsbeautiful/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,42 @@
+package com.lifeIsbeautiful.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setHeader("lhng-error-message", "Authentication");
+        // Build the JSON response
+        LocalDateTime dateTime = LocalDateTime.now();
+        String status = String.valueOf(HttpStatus.UNAUTHORIZED.value());
+        String message = (authException != null && authException.getMessage() != null) ? authException.getMessage() : "Unauthorized";
+        String path = request.getRequestURI();
+
+        String jsonResponse = String.format(
+                "{\n" +
+                        "    \"timestamp\": \"%s\",\n" +
+                        "    \"status\": %s,\n" +
+                        "    \"error\": \"%s\",\n" +
+                        "    \"path\": \"%s\"\n" +
+                        "}",
+                dateTime,status,message,path
+        );
+
+        // Set response headers and body
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write(jsonResponse);
+
+    }
+}
