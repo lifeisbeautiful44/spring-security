aa182b2d3a2dfa8ea69c4fb924d33a6eefb36f6b
lifeisbeautiful44 <bohara.srijansil44@gmail.com>
2024-12-10 22:17:58 +0545

    Csrf Configuration and token generation implmentation

--- section8/section8/springsec-cors-csrf/src/main/java/com/lifeIsbeautiful/config/TestSecurityConfig.java
+++ section8/section8/springsec-cors-csrf/src/main/java/com/lifeIsbeautiful/config/TestSecurityConfig.java
@@ -1,16 +1,21 @@
 package com.lifeIsbeautiful.config;
 
 import com.lifeIsbeautiful.exception.CustomAuthenticationEntryPoint;
+import com.lifeIsbeautiful.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -25,7 +30,11 @@ public class TestSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.cors(corsConfigurer -> corsConfigurer.configurationSource(new CorsConfigurationSource() {
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+
+        http.securityContext(contextConfig -> contextConfig.requireExplicitSave(false))
+                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration corsConfiguration = new CorsConfiguration();
@@ -37,8 +46,11 @@ public class TestSecurityConfig {
                         return corsConfiguration;
                     }
                 }))
+                .csrf(csrf -> csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+
+                .addFilterBefore(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .requiresChannel(rrc -> rrc.anyRequest().requiresInsecure()) //only HTTP
-                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans", "/user").authenticated()
                         .requestMatchers("/contact", "/notices", "/register").permitAll());
