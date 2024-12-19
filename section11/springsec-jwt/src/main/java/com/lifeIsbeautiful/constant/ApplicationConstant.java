fd024c1f528e361cedf9cc66d89e85f25f03f449
lifeisbeautiful44 <bohara.srijansil44@gmail.com>
2024-12-19 23:01:01 +0545

    Added implementation for the jwt token generator.

---
+++ section11/springsec-jwt/src/main/java/com/lifeIsbeautiful/constant/ApplicationConstant.java
@@ -0,0 +1,9 @@
+package com.lifeIsbeautiful.constant;
+
+public final class ApplicationConstant {
+
+    public final static String JWT_SECRET_KEY  = "JWT_SECRET";
+    public final static String JWT_DEFAULT_VALUE  = "asdsafesfrw3r9w4ertwegsdvsdv";
+    public final static String JWT_HEADER  = "AUTHORIZATION";
+
+}
--- section11/springsec-jwt/src/main/java/com/lifeIsbeautiful/filter/JWTTokenGeneratorFilter.java
+++ section11/springsec-jwt/src/main/java/com/lifeIsbeautiful/filter/JWTTokenGeneratorFilter.java
@@ -1,18 +1,51 @@
 package com.lifeIsbeautiful.filter;
 
+import com.lifeIsbeautiful.constant.ApplicationConstant;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            Environment env = getEnvironment();
+            if (env != null) {
+                String secret = env.getProperty(ApplicationConstant.JWT_SECRET_KEY,
+                        ApplicationConstant.JWT_DEFAULT_VALUE);
+
+                SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+                String jwtToken = Jwts.builder().issuer("Lhng bank").subject("JWT token")
+                        .claim("username", authentication.getPrincipal())
+                        .claim("authorities", authentication.getAuthorities().stream()
+                                .map(GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
+                        .issuedAt(new Date())
+                        .expiration(new Date((new Date().getTime() + 300000)))
+                        .signWith(secretKey)
+                        .compact();
+
+                response.setHeader(ApplicationConstant.JWT_HEADER, jwtToken);
+            }
+        }
     }
 
     //Note: If true then skip this filter.
