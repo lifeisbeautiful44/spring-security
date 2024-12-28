8d608abd1049ef0257fc8ab201a5b64025d18f11
lifeisbeautiful44 <bohara.srijansil44@gmail.com>
2024-12-28 23:12:55 +0545

    soical login implemenation setup

---
+++ section14/sprintsecOauth2/src/main/java/com/lifeisbeautiful/SprintsecOauth2Application.java
@@ -0,0 +1,13 @@
+package com.lifeisbeautiful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintsecOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintsecOauth2Application.class, args);
+	}
+
+}
---
+++ section14/sprintsecOauth2/src/test/java/com/lifeisbeautiful/SprintsecOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeisbeautiful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintsecOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
