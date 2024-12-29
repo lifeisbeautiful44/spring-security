88d9193807a57517688b0f50fe0a29fb4f9199f7
lifeisbeautiful44 <bohara.srijansil44@gmail.com>
2024-12-29 11:55:33 +0545

    Added setup for the client repository

--- section14/sprintsecOauth2/src/main/java/com/lifeisbeautiful/SpringsecOauth2Application.java
+++ section14/sprintsecOauth2/src/main/java/com/lifeisbeautiful/SpringsecOauth2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SprintsecOauth2Application {
+public class SpringsecOauth2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprintsecOauth2Application.class, args);
+		SpringApplication.run(SpringsecOauth2Application.class, args);
 	}
 
 }
---
+++ section14/sprintsecOauth2/src/main/java/com/lifeisbeautiful/controller/SecureController.java
@@ -0,0 +1,14 @@
+package com.lifeisbeautiful.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecureController {
+
+    @GetMapping("/secure")
+    public String securePage()
+    {
+        return "secure.html";
+    }
+}
